4472303e72d615524cdb465f6bf2c4fe35d1dcb8
Orbitium <sametdemiral26@gmail.com>
2022-10-27 15:10:01 +0300

    Fixed small issues, added reset name function

--- src/main/java/me/orbitium/command/NameCommandExecutor.java
+++ src/main/java/me/orbitium/command/NameCommandExecutor.java
@@ -1,120 +1,144 @@
-// 
-// Decompiled by Procyon v0.5.36
-// 
-
 package me.orbitium.command;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ArrayList;
-import org.bukkit.Bukkit;
 import me.clip.placeholderapi.PlaceholderAPI;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import me.orbitium.SimplyNicks;
-import org.bukkit.configuration.Configuration;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.entity.Player;
 
-public class NameCommandExecutor implements CommandExecutor
-{
-    Configuration config;
-    
-    public NameCommandExecutor() {
-        this.config = (Configuration)SimplyNicks.getInstance().getConfig();
-    }
-    
-    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+public class NameCommandExecutor implements CommandExecutor {
+    Configuration config = SimplyNicks.getInstance().getConfig();
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player;
         if (args.length == 1 && sender instanceof Player) {
-            final Player pSender = (Player)sender;
-            this.isColorPermissionsValid(sender, args[0]);
-            if (sender.hasPermission(this.config.getString("permissions.changeOwnName")) || (sender.isOp() && this.config.getBoolean("OPColorPermissionCheck"))) {
+            player = (Player)sender;
+            if (!sender.hasPermission(this.config.getString("permissions.changeOwnName")) && (!sender.isOp() || !this.config.getBoolean("OPColorPermissionCheck"))) {
+                sender.sendMessage(this.config.getString("messages.error.permissionErrorMessage"));
+            } else {
+                if (args[0].equals("reset")) {
+                    SimplyNicks.getDatabase().updatePlayerNameData(player, player.getName());
+                    player.setDisplayName(player.getName());
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, this.config.getString("messages.nickResettedMessage"))));
+                    return true;
+                }
+
+                this.isColorPermissionsValid(sender, args[0]);
                 if (!SimplyNicks.getCache().isNameAvailable(args[0])) {
                     sender.sendMessage(this.config.getString("messages.error.nameAlreadyInUseMessage"));
                     return true;
                 }
+
                 if (!this.isColorPermissionsValid(sender, args[0])) {
                     sender.sendMessage(this.config.getString("messages.error.colorPermissionErrorMessage"));
                     return true;
                 }
+
                 if (!this.isNameLenghtValid(sender, args[0])) {
                     return true;
                 }
-                SimplyNicks.getDatabase().updatePlayerNameData(pSender, args[0]);
-                pSender.setDisplayName(ChatColor.translateAlternateColorCodes('&', args[0]));
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(pSender, this.config.getString("messages.nickChangedSuccessfullyMessage"))));
+
+                SimplyNicks.getDatabase().updatePlayerNameData(player, args[0]);
+                player.setDisplayName(ChatColor.translateAlternateColorCodes('&', args[0]));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, this.config.getString("messages.nickChangedSuccessfullyMessage"))));
             }
-            else {
-                sender.sendMessage(this.config.getString("messages.error.permissionErrorMessage"));
-            }
-        }
-        else if (args.length == 2) {
-            if (!sender.hasPermission(this.config.getString("permissions.changeOthersName")) || (sender.isOp() && this.config.getBoolean("OPColorPermissionCheck"))) {
+        } else if (args.length == 2) {
+            if (!sender.hasPermission(this.config.getString("permissions.changeOthersName")) || sender.isOp() && this.config.getBoolean("OPColorPermissionCheck")) {
                 sender.sendMessage(this.config.getString("messages.error.permissionErrorMessage"));
                 return true;
             }
-            final Player player = Bukkit.getPlayer(args[0]);
+
+            player = Bukkit.getPlayer(args[0]);
+
+            if (args[1].equals("reset")) {
+                SimplyNicks.getDatabase().updatePlayerNameData(player, player.getName());
+                player.setDisplayName(player.getName());
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, this.config.getString("messages.nickResettedMessageByModerator"))));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, this.config.getString("messages.moderatorNickResetMessage"))));
+                return true;
+            }
+
             if (player == null) {
                 sender.sendMessage(this.config.getString("messages.error.playerCannotFoundErrorMessage"));
                 return true;
             }
+
             if (!SimplyNicks.getCache().isNameAvailable(args[1])) {
                 sender.sendMessage(this.config.getString("messages.error.nameAlreadyInUseMessage"));
                 return true;
             }
+
             if (!this.isColorPermissionsValid(sender, args[1])) {
                 sender.sendMessage(this.config.getString("messages.error.colorPermissionErrorMessage"));
                 return true;
             }
+
             if (!this.isNameLenghtValid(sender, args[1])) {
                 return true;
             }
+
             SimplyNicks.getDatabase().updatePlayerNameData(player, args[1]);
             player.setDisplayName(ChatColor.translateAlternateColorCodes('&', args[1]));
             player.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, this.config.getString("messages.nickChangedByModeratorMessage"))));
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player, this.config.getString("messages.moderatorNickChangedMessage"))));
         }
+
         return true;
     }
-    
-    boolean isColorPermissionsValid(final CommandSender sender, final String name) {
+
+    boolean isColorPermissionsValid(CommandSender sender, String name) {
         boolean valid = true;
         boolean set = false;
-        final List<String> colors = new ArrayList<String>();
-        for (final char c : name.toCharArray()) {
+        List<String> colors = new ArrayList();
+        char[] var6 = name.toCharArray();
+        int var7 = var6.length;
+
+        for(int var8 = 0; var8 < var7; ++var8) {
+            char c = var6[var8];
             if (c == '&') {
                 set = true;
-            }
-            else if (set) {
+            } else if (set) {
                 set = false;
                 colors.add("&" + c);
             }
         }
-        for (final String color : colors) {
+
+        Iterator var10 = colors.iterator();
+
+        while(var10.hasNext()) {
+            String color = (String)var10.next();
             if (!SimplyNicks.colors.contains(color)) {
                 return sender.isOp() && !this.config.getBoolean("OPColorPermissionCheck");
             }
-            if (sender.isPermissionSet(this.config.getString("permissions.accessColors"))) {
-                continue;
+
+            if (!sender.isPermissionSet(this.config.getString("permissions.accessColors"))) {
+                valid = false;
             }
-            valid = false;
         }
-        return (sender.isOp() && !this.config.getBoolean("OPColorPermissionCheck")) || valid;
+
+        return sender.isOp() && !this.config.getBoolean("OPColorPermissionCheck") || valid;
     }
-    
-    boolean isNameLenghtValid(final CommandSender sender, String name) {
+
+    boolean isNameLenghtValid(CommandSender sender, String name) {
         if (!this.config.getBoolean("countColorCodesAsCharacter")) {
             name = ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', name));
         }
+
         if (name.length() < 3) {
             sender.sendMessage(this.config.getString("messages.error.shortNickErrorMessage"));
             return false;
-        }
-        if (name.length() > 16) {
+        } else if (name.length() > 16) {
             sender.sendMessage(this.config.getString("messages.error.longNickErrorMessage"));
             return false;
+        } else {
+            return true;
         }
-        return true;
     }
 }
--- src/main/java/me/orbitium/event/PlayerEvents.java
+++ src/main/java/me/orbitium/event/PlayerEvents.java
@@ -1,39 +1,37 @@
-// 
-// Decompiled by Procyon v0.5.36
-// 
-
 package me.orbitium.event;
 
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.EventHandler;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.ChatColor;
-import me.clip.placeholderapi.PlaceholderAPI;
 import java.util.UUID;
+import me.clip.placeholderapi.PlaceholderAPI;
 import me.orbitium.SimplyNicks;
 import org.bukkit.Bukkit;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 
-public class PlayerEvents implements Listener
-{
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerLogin(final PlayerLoginEvent event) {
-        Bukkit.getScheduler().runTaskLater((Plugin)SimplyNicks.getInstance(), () -> {
-
+public class PlayerEvents implements Listener {
+    @EventHandler(
+            priority = EventPriority.HIGHEST
+    )
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        Bukkit.getScheduler().runTaskLater(SimplyNicks.getInstance(), () -> {
             if (!SimplyNicks.getCache().isNameAvailable(event.getPlayer().getUniqueId().toString(), event.getPlayer().getName())) {
                 OfflinePlayer fakeNamedPlayer = Bukkit.getOfflinePlayer(UUID.fromString(SimplyNicks.getCache().getUUIDByName(event.getPlayer().getName())));
                 if (fakeNamedPlayer.isOnline()) {
                     fakeNamedPlayer.getPlayer().setDisplayName(fakeNamedPlayer.getName());
                     fakeNamedPlayer.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(fakeNamedPlayer.getPlayer(), SimplyNicks.getInstance().getConfig().getString("messages.error.nickFixedByOwnerMessage"))));
                 }
+
                 SimplyNicks.getDatabase().updatePlayerNameData(fakeNamedPlayer.getUniqueId().toString(), fakeNamedPlayer.getName());
             }
+
             String name = SimplyNicks.getDatabase().getPlayerNameData(event.getPlayer().getUniqueId().toString());
             if (name == null) {
                 name = event.getPlayer().getName();
             }
+
             SimplyNicks.getCache().addNewName(event.getPlayer().getUniqueId().toString(), event.getPlayer().getName());
             event.getPlayer().setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
             event.getPlayer().setCustomNameVisible(true);
