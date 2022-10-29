30ef7951ed3925ad566e2b83effa8d41e267c3d7
Netherwhal <mail@netherwhal.com>
2022-10-29 04:25:51 +0200

    remove dupe

--- src/main/java/net/simplyvanilla/simplynicks/util/NickUtil.java
+++ src/main/java/net/simplyvanilla/simplynicks/util/NickUtil.java
@@ -5,10 +5,6 @@ import org.bukkit.entity.Player;
 
 public class NickUtil {
     public static void applyNick(Player player, String nick) {
-        // ensure nick does not overflow
-        nick = nick.replaceAll("(&r)+$", "");
-        nick += "&r";
-
         player.displayName(LegacyComponentSerializer.legacyAmpersand().deserialize(nick));
     }
 }
